687e2e9f24599e04296a420cb464ec09f2c037cb
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-10-08 09:34:17 +0530

    Add files via upload

---
+++ Random_Name_generator.py
@@ -0,0 +1,24 @@
+import random
+
+
+syllables = ["ra", "mi", "do", "so", "la", "ti", "di", "ze", "fi", "po", "ni", "ko"]
+endings = ["an", "el", "or", "ius", "ious", "ara", "ella", "inda", "oria", "ette"]
+
+def generate_random_name():
+    name_length = random.randint(2, 3)  
+
+    name = ""
+    for _ in range(name_length):
+        name += random.choice(syllables)
+    name += random.choice(endings)
+
+    return name.capitalize()
+
+if __name__ == "__main__":
+    num_names = int(input("Enter the number of random names to generate: "))
+    
+    random_names = [generate_random_name() for _ in range(num_names)]
+
+    print("Random Names:")
+    for i, name in enumerate(random_names, 1):
+        print(f"{i}. {name}")
